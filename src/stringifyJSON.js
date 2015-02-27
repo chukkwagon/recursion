3f0a6e1dfa8f6a5cddeef140b18ab61819937170
Charlie McBride <mcbride.charles4@gmail.com>
2015-02-27 01:25:39 -0600

    "Final edits to stringifyJSON that pass all mocha tests"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,37 +1,40 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
-  var t = typeof(obj);
+  var t = typeof(obj)
 
   //Simple data types
   if (t != 'object' || obj === null ) {
   	if (t==='string') {
   		return '"'+obj+'"'
   	}
-  	return String(obj);
+  	return String(obj)
+
+  //Objects and arrays	
   } else {
-  	var n, v;
-  	var json = [];
-  	var arr = (obj && obj.constructor === Array);
+  	var n, v
+  	var arr = false
+  	var json = []
+
+  	if (obj.constructor === Array) {
+  		arr = true;
+  	}
 
   	for (n in obj) {
-  		v = obj[n];
-  		t = typeof(v);
+  		v = obj[n]
+  		t = typeof(v)
 
-  		if (t=='string') {
-  			v = '"' + v + '"';
-  		} else if (t=='object' && v !== null) {
-  			v = stringifyJSON(v);
-  			console.log(v);
+  		if (v===undefined || t==='function') { 
+  			return '{}';
   		}
-  		json.push((arr ? "" : '"' + n + '":') + String(v));
+
+  		if (t==='string') {
+  			v = '"' + v + '"'
+  		} else if (t==='object' && v!== null) {
+  			v = stringifyJSON(v)
+  		}
+  		json.push((arr ? "" : '"' + n + '":') + String(v))
   	}
   	return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
   }
 };
 
-stringifyJSON({"a":{"b":"c"}});
