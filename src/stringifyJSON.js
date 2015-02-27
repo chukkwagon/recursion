86be94c3cab227b70c1510dd68eb147edcab7bac
Charlie McBride <mcbride.charles4@gmail.com>
2015-02-27 00:43:15 -0600

    "Implemented stringifyJSON.js that runs in my browser js console with any input, but doesn't pass the tests for some reason"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var t = typeof(obj);
+
+  //Simple data types
+  if (t != 'object' || obj === null ) {
+  	if (t==='string') {
+  		return '"'+obj+'"'
+  	}
+  	return String(obj);
+  } else {
+  	var n, v;
+  	var json = [];
+  	var arr = (obj && obj.constructor === Array);
+
+  	for (n in obj) {
+  		v = obj[n];
+  		t = typeof(v);
+
+  		if (t=='string') {
+  			v = '"' + v + '"';
+  		} else if (t=='object' && v !== null) {
+  			v = stringifyJSON(v);
+  			console.log(v);
+  		}
+  		json.push((arr ? "" : '"' + n + '":') + String(v));
+  	}
+  	return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+  }
 };
+
+stringifyJSON({"a":{"b":"c"}});
